5d014cf979360725d46b36dc5d6380f505863043
amisha <amishaadwani0023@gmail.com>
2023-10-17 23:35:43 +0530

    models created

---
+++ server/models/Chat.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const ChatSchema = new Schema({
+    user_name:{
+        type: String,
+        required: true
+    },
+   message_text:{
+    type: String,
+    required: true
+   },
+   date:{
+    type: Date,
+    default: Date.now
+   }
+})
+
+module.exports = mongoose.model('chat',ChatSchema);
