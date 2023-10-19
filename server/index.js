a98b9ff0f6e29f4a164c80af698cfe2238ecb882
amisha <amishaadwani0023@gmail.com>
2023-10-19 23:35:40 +0530

    socket.io integrated

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
-const connectToMongo = require('./db');
-const express = require('express')
+const connectToMongo = require("./db");
+const http = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+
 connectToMongo();
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Specify the allowed origin
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected: ${socket.id}`);
+  console.log("Connection established");
+});
+
+server.listen(3001, () => {
+  console.log("Example app listening at http://localhost:3001");
+});
