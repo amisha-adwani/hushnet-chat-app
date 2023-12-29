527de6b9ac16ab308c8bcd8054824516264cc937
amisha <amishaadwani0023@gmail.com>
2023-12-29 16:42:17 +0530

    allowed origin

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors())
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://hushnet-frontend.onrender.com", "http://localhost:3000"],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
