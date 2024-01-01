b2a043b213900fddab133421a84601eb58bc7324
amisha <amishaadwani0023@gmail.com>
2024-01-01 22:51:16 +0530

    changes

--- server/routes/room.js
+++ server/routes/room.js
@@ -2,9 +2,14 @@ const express = require("express");
 const Room = require("../models/Room");
 const router = express.Router();
 
-router.get('/', async(req,res) =>{
-    const rooms = await Room.find()
-    res.json({rooms}) 
-})
-
+router.get("/", async (req, res) => {
+    try {
+      const rooms = await Room.find();
+      console.log("Fetched rooms:", rooms);
+      res.json({ rooms });
+    } catch (error) {
+      console.error("Error fetching rooms:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 module.exports = router;
