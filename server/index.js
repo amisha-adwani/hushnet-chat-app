3bfa5ea89d87840a5b25e4e87478384c559283ad
amisha <amishaadwani0023@gmail.com>
2024-01-27 17:10:32 +0530

    final

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://hushnet.onrender.com",
     methods: ["GET", "POST"],
   },
 });
